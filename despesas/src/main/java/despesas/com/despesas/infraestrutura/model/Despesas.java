448f9426cc314c671c93a45c778ed06ee94c860d
Carlos Henrique <carlinhosbackend@gmail.com>
2025-09-19 20:14:43 -0300

    feat: primeiro commit

---
+++ despesas/src/main/java/despesas/com/despesas/infraestrutura/model/Despesas.java
@@ -0,0 +1,16 @@
+package despesas.com.despesas.infraestrutura.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Despesas {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String descricao;
+}
